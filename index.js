f38b4a3ce1dc3528592c819a4025a731349b437a
chuckmlafleur <clafleur2020@fau.edu>
2024-07-03 13:38:34 -0400

    Initial commit with project files

---
+++ index.js
@@ -0,0 +1,14 @@
+function drawTriangle(triangleSize) {
+    for (let i = 1; i <= triangleSize; i++) {
+        let line = '';
+        for (let j = 0; j < i; j++) {
+            line += '*';
+        }
+        console.log(line);
+    }   
+}
+console.log("Testing drawTriangle()...");
+// TODO: Test drawTriangle() with different arguments
+drawTriangle(4);
+// Do NOT remove the following line
+export default drawTriangle;
